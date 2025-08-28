c080cd6284c0b1e6fb4130a5146c381381aa8493
Yu Chin Fabian Lim <flim@sg.ibm.com>
2025-08-28 23:38:57 +0000

    add optimized kernel test
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- methods_test.py
+++ methods_test.py
@@ -1,14 +1,19 @@
 import torch
 from copy import deepcopy
 
-from utils_methods import set_seed, random_instance, blockwise_ua_test_harness
+from utils_methods import (
+    set_seed, random_instance, 
+    blockwise_ua_test_harness,
+    blockwise_ua_test_harness2,
+)
 from universal_attention_autograd import simplest_implementation
 
-def test_ua_slow(
+def test_ua_kernels(
     b: int = 1, # batch
     l: int = 32, # sequence
     h: int = 2, # heads
     d: int = 4, # dim
+    method: str = 'slow',
     rtol: float = 1e-4,
     atol: float = 1e-4,
 ):
@@ -26,21 +31,32 @@ def test_ua_slow(
     out_ref, denom_ref = simplest_implementation(q, k, v, static_src, static_dest)
 
     # run the slow kernel
-    out, denom = blockwise_ua_test_harness(q2, k2, v2, static_src, static_dest, chunk_size=l)
-    out = out.squeeze(-1).squeeze(-3) # b, h, l, d
-    denom = denom.squeeze(-1).squeeze(-2) # b, h, l
+    if method == 'slow':
+        out, denom = blockwise_ua_test_harness(
+            q2, k2, v2, static_src, static_dest, 
+            chunk_size=l,
+        )
+        out = out.squeeze(-1).squeeze(-3) # b, h, l, d
+        denom = denom.squeeze(-1).squeeze(-2) # b, h, l
+    elif method == 'optimized':
+        out, denom = blockwise_ua_test_harness2(
+            q2, k2, v2, static_src, static_dest, 
+        )
 
     # backward
     out_ref.norm().backward()
     out.norm().backward()
 
-    for a, b in [
+    test_cases = [
         (out_ref, out),
-        (denom_ref, denom),
         (q.grad, q2.grad),
         (k.grad, k2.grad),
         (v.grad, v2.grad),
-    ]:
+    ]
+    if denom is not None:
+        test_cases.append((denom_ref, denom))
+
+    for a, b in test_cases:
         torch.testing.assert_close(
             a, b, atol=atol, rtol=rtol
         )
